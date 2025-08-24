cb68da3fd4e31df463917c0c6238a6b6978d049f
SauravAcharya23 <sauravacharya236@gmail.com>
2025-08-24 15:54:13 +0545

    Created 50 particles

--- lib/animated_screen.dart
+++ lib/animated_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animation/particle_system.dart';
 import 'package:flutter/material.dart';
 
 class AnimatedParticleScreen extends StatefulWidget {
@@ -11,10 +12,24 @@ class _AnimatedParticleScreenState extends State<AnimatedParticleScreen> with Si
 
   late AnimationController _particleController;
 
+  List<Particle> particles = [];
+  final int particleCount = 50;
+
   @override
   void initState() {
     super.initState();
     _particleController = AnimationController(vsync: this, duration: Duration(seconds: 20))..repeat();
+    _initializeParticles();
+  }
+
+  void _initializeParticles() {
+    particles = List.generate(particleCount, (index) => Particle());
+  }
+
+  @override
+  void dispose() {
+    _particleController.dispose();
+    super.dispose();
   }
   @override
   Widget build(BuildContext context) {
