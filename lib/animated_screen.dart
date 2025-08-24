83a8da11ed1145c97229a95e399cf86f2683a1e9
SauravAcharya23 <sauravacharya236@gmail.com>
2025-08-24 15:48:08 +0545

    Clean the code

---
+++ lib/animated_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatedParticleScreen extends StatefulWidget {
+  const AnimatedParticleScreen({super.key});
+
+  @override
+  State<AnimatedParticleScreen> createState() => _AnimatedParticleScreenState();
+}
+
+class _AnimatedParticleScreenState extends State<AnimatedParticleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
